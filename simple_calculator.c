6001640fd71cbc40bd9f44af70a9901049012df6
Giovani-Simple-Dev <167205762+Giovani-Simple-Dev@users.noreply.github.com>
2024-10-24 13:44:48 -0300

    Update simple_calculator.c

--- simple_calculator.c
+++ simple_calculator.c
@@ -1,57 +1,58 @@
 #include <stdio.h>
 
-int soma(int a, int b) {
-    return a + b;
-}
-
-int subtrai(int a, int b) {
-    return a - b;
-}
-
-int multiplicar(int a, int b) {
-    return a * b;
-}
-
-int dividir(int a, int b){
-    return a / b;
-}
-
-int main() {
-        int num1, num2, opcao, resultado;
-
-        printf("Digite o Primeiro Número: ");
-        scanf("%d", &num1);
-
-        printf("Digite o Segundo Número: ");
-        scanf("%d", &num2);
-
-        printf("Escolha uma opção: \n");
-        printf("1. Somar\n");
-        printf("2. Subtrair\n");
-        printf("3. Multiplicar\n");
-        printf("4. Divisão\n");
-        printf("Opção: ");
-        scanf("%d", &opcao);
-
-        switch (opcao)
-        {
+int main(int option, float a, float b){
+    
+    printf("Painel de Opções: \n");
+    printf("0) Sair\n");
+    printf("1) Somar\n");
+    printf("2) Subtrair\n");
+    printf("3) Multiplicar\n");
+    printf("4) Dividir\n");
+    printf("Option: ");
+    scanf("%d", &option);
+    
+    
+    switch( option ) 
+    {
+        case 0:
+        system("cls || clear");
+        printf("Você escolheu sair");
+        break;
+        
         case 1:
-            resultado = soma(num1,num2);
-            printf("Resultado: %d\n", resultado);
+        system("cls || clear");
+        printf("Digite o Primeiro Valor: ");
+        scanf("%d", &a);
+        printf("Digite o Segundo Valor: ");
+        scanf("%d", &b);
+        printf("O Resultado é: %d\n", a + b);
+        return 0;
+        
         case 2:
-            resultado = subtrai(num1,num2);
-            printf("Resultado: %d\n", resultado);
+        system("cls || clear");
+        printf("Digite o Primeiro Valor: ");
+        scanf("%d", &a);
+        printf("Digite o Segundo Valor: ");
+        scanf("%d", &b);
+        printf("O resultado é: %d\n", a - b);
+        return 0;
+        
         case 3:
-            resultado = multiplicar(num1,num2);
-            printf("Resultado: %d\n", resultado);
+        system("cls || clear");
+        printf("Digite o Primeiro Valor: ");
+        scanf("%d", &a);
+        printf("Digite o Segundo Valor: ");
+        scanf("%d", &b);
+        printf("O Resultado é: %d\n", a * b);
+        return 0;
+        
         case 4:
-            resultado = dividir(num1, num2);
-            if (num2 != 0){
-            printf("Resultado: %d\n", resultado);
-        default:
-            printf("Opção Inválida\n");
-            break;
-            }
-            return 0;
+        system("cls || clear");
+        printf("Digite o Primeiro Valor: ");
+        scanf("%f", &a);
+        printf("Digite o Segundo Valor: ");
+        scanf("%f", &b);
+        printf("O Resultado é: %f\n", a / b);
+        return 0;
     }
 }
